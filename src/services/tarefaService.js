9adde3a04927b5c93010314f7d7c06683ad7969f
igordcs <igorcostamdt@gmail.com>
2024-06-07 11:25:26 -0300

    Criado métodos para alterar e deletar tarefa

--- src/services/tarefaService.js
+++ src/services/tarefaService.js
@@ -9,4 +9,23 @@ async function pegarTarefas() {
     }
 }
 
-export default {pegarTarefas};
+async function editarTarefa(id, tarefa) {
+    try {
+        const {nome, descricao, prioridade, finalizada, dataTermino} = tarefa
+        const {data} = await api.put(`tarefa/alterar/${id}`, {nome, descricao, prioridade, finalizada, dataTermino})
+        return data
+    } catch (error) {
+        return error
+    }
+}
+
+async function deletarTarefa(id) {
+    try {
+        const {data} = await api.delete(`tarefa/deletar/${id}`)
+        return data
+    } catch (error) {
+        return error
+    }
+}
+
+export default {pegarTarefas, editarTarefa, deletarTarefa};
