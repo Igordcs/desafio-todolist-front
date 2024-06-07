92c621d710598da6f6b947c73c45eba0745aca88
igordcs <igorcostamdt@gmail.com>
2024-06-07 10:32:03 -0300

    tela de listagem de Tarefas

---
+++ src/components/tarefa/index.js
@@ -0,0 +1,62 @@
+import {FaEdit, FaTrashAlt, FaStopwatch} from "react-icons/fa";
+
+import './index.css'
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { tarefaContext } from "../../context/tarefaContext";
+
+function Tarefa({tarefa}) {
+    const {handleSelecionarTarefa} = useContext(tarefaContext);
+    const [isDescricaoVisivel, setisDescricaoVisivel] = useState(false);
+
+    function handleDescricaoVisivel(e) {
+        e.preventDefault()
+        setisDescricaoVisivel(!isDescricaoVisivel)
+    }
+
+    const colors = {
+        ALTA: "#E83F5B",
+        MEDIA: "#cbd633",
+        BAIXA: "#DCDDE0"
+    }
+    
+    return (
+        <button className="tarefaWrapper" onClick={handleDescricaoVisivel}>
+            <div className="tarefaBasicInfo">
+                <div className="tarefaTitleWrapper">
+                    <h3>{tarefa.nome}</h3>
+                </div>
+                <div className="prioridadeWrapper">
+                    <h4 style={{color: colors[tarefa.prioridade]}}>{tarefa.prioridade}</h4>
+                    <FaStopwatch color={colors[tarefa.prioridade]} />
+                </div>
+                <div>
+                    <p>{tarefa.finalizada ? "Finalizada" : "A fazer"}</p>
+                </div>
+            </div>
+            {isDescricaoVisivel && (
+                <div className="tarefaMoreAction">
+                    <div className="tarefaDescricao">
+                        <h3>Descrição: </h3>
+                        <p>{tarefa.descricao}</p>
+                    </div>
+                    <div className="tarefaActions">
+                        <Link 
+                            to="/editarTarefa" 
+                            className="buttonEditar" 
+                            onClick={() => handleSelecionarTarefa(tarefa)}>
+                            Editar tarefa
+                            <FaEdit size={"12px"} color="#fff" />
+                        </Link>
+                        <button className="buttonExcluir">
+                            Excluir tarefa
+                            <FaTrashAlt size={"12px"} color="#fff" />
+                        </button>
+                    </div>
+                </div>
+            )}
+        </button>
+    )
+}
+
+export default Tarefa;
--- src/pages/listarTarefas.js
+++ src/pages/listarTarefas.js
@@ -1,5 +1,41 @@
+import { useContext, useEffect } from "react";
+import { FaSearch, FaStopwatch} from "react-icons/fa";
+
+import '../styles/ListarTarefas.css'
+import Tarefa from "../components/tarefa";
+import {tarefaContext} from '../context/tarefaContext';
+
 function ListarTarefas () {
-    return <h1>ListarTarefas</h1>
+    const {carregando, tarefas, getTarefas} = useContext(tarefaContext)
+
+    useEffect(() => {
+        getTarefas();
+    }, [])
+    
+    return (
+        <div id="page-listarTarefas">
+            <header id="page-header">
+                <div>
+                    <h1>Todolist</h1>
+                </div>
+            </header>
+            <div className="centered">
+                <main className="listContainer">
+                    <h2>Lista de tarefas</h2>
+                    <div className="searchContainer">
+                        <div>
+                            <input type="Text" placeholder="Procure uma tarefa"/>
+                            <FaSearch color='#000' size={"24px"} />
+                        </div>
+                    </div>
+                    <div className="tarefaContainer">
+                        {carregando && <p>Carregando</p>}
+                        {!carregando && tarefas.map((tarefa) => <Tarefa tarefa={tarefa}/>)}
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
 }
 
 export default ListarTarefas;
