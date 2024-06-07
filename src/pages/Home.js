8942ec7ee8dbc2ed3b0b8b34175d4e89d8fd3e34
igordcs <igorcostamdt@gmail.com>
2024-06-07 08:42:11 -0300

    Adicionado tela home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
+import {Link} from 'react-router-dom'
+
+import '../styles/Home.css'
+
 function Home () {
-    return <h1>Home</h1>
+    return (
+        <div id="page-home">
+            <div className='mainFrame'>
+                <Link to={"/listarTarefas"}>Listagem de tarefas</Link>
+                <Link to={"/cadastrarMembro"}>Cadastrar Membro</Link>
+            </div>
+            <div className='heroImg'>
+                <h1>Todolist</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
