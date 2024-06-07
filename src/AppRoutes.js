3cee798011bd35df61257eb81b82358aa0c17e35
igordcs <igorcostamdt@gmail.com>
2024-06-07 12:49:11 -0300

    rota de login

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -9,11 +9,13 @@ import EditarTarefa from './pages/editarTarefa';
 import CadastrarTarefa from './pages/cadastrarTarefa';
 import ListarTarefas from './pages/listarTarefas';
 import CadastrarMembro from './pages/cadastrarMembro';
+import LoginMembro from './pages/login';
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path='/' Component={Home} />
+            <Route path='/login' Component={LoginMembro} />
             <Route path='/editarTarefa' Component={EditarTarefa} />
             <Route path='/cadastrarTarefa' Component={CadastrarTarefa} />
             <Route path='/listarTarefas' Component={ListarTarefas} />
